0c657666d6f76288d3a186dfef78d8ddc8f7b879
Muhamedsivic <muhamedsivic@gmail.com>
2023-08-17 00:35:01 +0200

    prettier style reformat

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
-const express = require('express');
-const { findMaxId } = require('./utils/utils');
+const express = require("express");
+const { findMaxId } = require("./utils/utils");
 
-const app = express()
+const app = express();
 app.use(express.json());
 
 const employees = [
-    {
-        id: "1",
-        name: "muhamed",
-        surname: "sivic"
-    },
-    {
-        id: "2",
-        name: "ahmed",
-        surname: "krdzalic"
-    }
-]
+  {
+    id: "1",
+    name: "muhamed",
+    surname: "sivic",
+  },
+  {
+    id: "2",
+    name: "ahmed",
+    surname: "krdzalic",
+  },
+];
 
-app.get('/employees', (req, res) => {
-    res.json(employees)
-  })
+app.get("/employees", (req, res) => {
+  res.json(employees);
+});
 
-app.get('/employees/:id', (req, res) => {
-    let id = req.params.id
-    let employee = employees.find((emp)=>emp.id === id)
-    res.json(employee)
-})
+app.get("/employees/:id", (req, res) => {
+  let id = req.params.id;
+  let employee = employees.find((emp) => emp.id === id);
+  res.json(employee);
+});
 
-app.post('/employees', (req, res) => {
-    let employee = req.body
+app.post("/employees", (req, res) => {
+  let employee = req.body;
 
-    employee.id = (findMaxId(employees)+1).toString()
+  employee.id = (findMaxId(employees) + 1).toString();
 
-    employees.push(employee)
+  employees.push(employee);
 
-    res.json(employees)
-})
+  res.json(employees);
+});
 
 app.listen(3000, () => {
-    console.log(`Example app listening on port 3000`)
-})
+  console.log(`Example app listening on port 3000`);
+});
